fb2fbd94a72a002b3388ded608d5bb4298b48c49
Darkgoatie <81323822+Darkgoatie@users.noreply.github.com>
2021-07-20 23:12:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,76 +28,88 @@ import Dono from './Components/Cmds/Donation';
 import Prefix from './Components/Cmds/Prefix';
 
 ReactDOM.render(
-<React.StrictMode>
-  <Router>
-    <Switch>  
-      <Route exact path="/home">
-        <Home />
-      </Route>
-      <Route exact path="/dependencies">
-        <Deps />
-      </Route>
-      <Route exact path="/features">
-        <Features />
-      </Route>
-      <Route path="/commands">
-        <Switch>
-        <Route exact path="/commands/all">
-          <AllCmds />
+  <React.StrictMode>
+    <Router>
+      <Switch>
+        <Route exact path="/home">
+          <Home />
         </Route>
-        <Route exact path="/commands/auction">
-          <Auction />
+        <Route exact path="/dependencies">
+          <Deps />
         </Route>
-        <Route exact path="/commands/giveaway">
-          <Giveaway />
+        <Route exact path="/features">
+          <Features />
         </Route>
-        <Route exact path="/commands/lottery">
-          <Lottery />
+        <Route path="/commands">
+          <Switch>
+            <Route exact path="/commands/all">
+              <AllCmds />
+            </Route>
+            <Route exact path="/commands/auction">
+              <Auction />
+            </Route>
+            <Route exact path="/commands/giveaway">
+              <Giveaway />
+            </Route>
+            <Route exact path="/commands/lottery">
+              <Lottery />
+            </Route>
+            <Route exact path="/commands/vouch">
+              <Vouch />
+            </Route>
+            <Route exact path="/commands/timer">
+              <Timer />
+            </Route>
+            <Route exact path="/commands/stats">
+              <Stats />
+            </Route>
+            <Route exact path="/commands/role">
+              <Role />
+            </Route>
+            <Route exact path="/commands/user">
+              <User />
+            </Route>
+            <Route exact path="/commands/message">
+              <Message />
+            </Route>
+            <Route exact path="/commands/channel">
+              <Channel />
+            </Route>
+            <Route exact path="/commands/server">
+              <Server />
+            </Route>
+            <Route exact path="/commands/invite">
+              <Invite />
+            </Route>
+            <Route exact path="/commands/donation">
+              <Dono />
+            </Route>
+            <Route exact path="/commands/prefix">
+              <Prefix />
+            </Route>
+            <Route>
+              <Redirect to="/commands/all" />
+            </Route>
+          </Switch>
         </Route>
-        <Route exact path="/commands/vouch">
-          <Vouch />
+        <Route path="/invite">
+          <meta http-equiv="refresh" content="0;URL='https://discord.com/oauth2/authorize?client_id=%20805537268349665290&permissions=8&scope=bot%20applications.commands'" />
         </Route>
-        <Route exact path="/commands/timer">
-          <Timer />
+        <Route path="/vote">
+          <meta http-equiv="refresh" content="0;URL='https://top.gg/bot/805537268349665290/vote'" />
         </Route>
-        <Route exact path="/commands/stats">
-          <Stats />
+        <Route path="/join">
+          <meta http-equiv="refresh" content="0;URL='https://discord.gg/Dn3VJmP3Ba'" />
         </Route>
-        <Route exact path="/commands/role">
-          <Role />
-        </Route>
-        <Route exact path="/commands/user">
-          <User />
-        </Route>
-        <Route exact path="/commands/message">
-          <Message />
-        </Route>
-        <Route exact path="/commands/channel">
-          <Channel />
-        </Route>
-        <Route exact path="/commands/server">
-          <Server />
-        </Route>
-        <Route exact path="/commands/invite">
-          <Invite />
-        </Route>
-        <Route exact path="/commands/donation">
-          <Dono />
-        </Route>
-        <Route exact path="/commands/prefix">
-          <Prefix />
+        <Route path="/premium">
+          <meta http-equiv="refresh" content="0;URL='https://patreon.com/aether'" />
         </Route>
         <Route>
-          <Redirect to="/commands/all" />
+          <NoMatch />
         </Route>
-        </Switch>
-      </Route>
-      <Route>
-        <NoMatch />
-      </Route>
-    </Switch>
-  </Router>
-</React.StrictMode>,
+      </Switch>
+    </Router>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
